56a67e0426b9d23453db1303b7cb0762074f2503
essemlalioussama <53407768+essemlalioussama@users.noreply.github.com>
2019-11-24 13:36:45 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,16 +4,16 @@ host = "192.168.1.6"
 port = 8080
 s.connect((host,port))
 print("connected ....")
-#receive size of the file
+#receive size of the picture
 
 size = s.recv(4096).decode()
 print(int(size))
 time.sleep(1)
-#receive the file
+#receive the picture
 
-filename = input("please enter a new name for the file : ")
-file = open(filename,'wb')
-file_data = s.recv(int(size))
-file.write(file_data)
-file.close()
-print("file has been received successfully")
+picturename = input("please enter a new name for the picture : ")
+picture = open(picturename,'wb')
+picture_data = s.recv(int(size))
+picture.write(picture_data)
+picture.close()
+print("picture has been received successfully")
